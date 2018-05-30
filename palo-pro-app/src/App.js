d363e265947e381a24e07f65d94e375724c4b2a6
Juan Palomino <juniorpalo16@gmail.com>
2018-05-29 19:17:21 -0600

    i think this is number one

--- palo-pro-app/src/App.js
+++ palo-pro-app/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
+      <nav>
+        <Link to ='/'> Home </Link>
+      </nav>
+      <Route exact path='/' component={Home}/>
       </div>
+      </Router>
     );
   }
 }
